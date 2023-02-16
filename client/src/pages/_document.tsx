ee0bcd2b03971fb6f35b5033ce872af7170f1b92
rama <akbar.6b@gmail.com>
2023-02-16 23:11:58 +0700

    fix setup

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="min-h-screen font-serif bg-white text-slate-900 antialiased">
+      <body className="min-h-screen font-sans bg-white text-slate-900 antialiased">
         <Main />
         <NextScript />
       </body>
---
+++ client/src/pages/about.tsx
@@ -0,0 +1,10 @@
+import CustomHead from "@/components/customHead";
+
+export default function about() {
+  return (
+    <>
+      <CustomHead title="RM Blog - About" />
+      <div>about</div>
+    </>
+  );
+}
