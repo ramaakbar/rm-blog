d92335aeaafde5f8729ca60e083fd38ea0ec1bcd
rama <akbar.6b@gmail.com>
2023-03-23 16:52:20 +0700

    update nav, add temp card and change var color

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -4,15 +4,17 @@ import ThemeSwitch from "./themeSwitch";
 
 export default function Navbar() {
   return (
-    <header className="mx-auto flex max-w-4xl justify-between p-4">
-      <Link href={"/"}>RM Blog</Link>
-      <nav className="flex space-x-3">
-        <div>User</div>
-        <div>dropdown</div>
-        <ThemeSwitch />
-        <Link href={"/login"}>Login</Link>
-        <Link href={"/register"}>Register</Link>
-      </nav>
+    <header className="p-4 border-b border-b-line">
+      <div className="mx-auto flex max-w-4xl justify-between">
+        <Link href={"/"}>RM Blog</Link>
+        <nav className="flex space-x-3">
+          <div>User</div>
+          <div>dropdown</div>
+          <ThemeSwitch />
+          <Link href={"/login"}>Login</Link>
+          <Link href={"/register"}>Register</Link>
+        </nav>
+      </div>
     </header>
   );
 }
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,9 @@
+import Image from "next/image";
+
 import CustomHead from "@/components/customHead";
 import Layout from "@/components/layout";
 
 export default function Home() {
-  const test:string = 1;
-
-  const printTest = () => {
-    console.log(test)
-  }
   return (
     <>
       <CustomHead title="RM Blog" />
@@ -15,12 +12,27 @@ export default function Home() {
           <h3>Search</h3>
           <input type="text" placeholder="search" />
         </div>
-        <div className="border-b flex justify-between">
+        <div className="border-b border-b-line flex justify-between mb-5 pb-2">
           <h2>Posts (5)</h2>
           <div className="flex">
             <div>asc</div>
             <div>desc</div>
           </div>
+        </div>
+        <div className="space-y-5">
+          {[...Array(4)].map((test, i) => (
+            <div key={i} className="bg-base-2 border border-overlay">
+              <Image
+                src="https://images.unsplash.com/photo-1678971062140-2dca8fd40db7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt=""
+                width={500}
+                height={500}
+                className="aspect-video w-full"
+              />
+              <h3 className="font-bold text-lg">test</h3>
+              <span className="text-base-content-3">January 5, 2023</span>
+            </div>
+          ))}
         </div>
       </Layout>
     </>
