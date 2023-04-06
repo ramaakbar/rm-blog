7eae5ba2723229c652664c26de7573dbdc4fd8b4
rama <akbar.6b@gmail.com>
2023-04-06 22:47:23 +0700

    still fixing cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,7 @@ import {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://blog.ramaakbar.xyz'],
+    origin: ['http://localhost:3000', 'https://ramaakbar.xyz'],
     credentials: true,
   });
   app.use(helmet());
